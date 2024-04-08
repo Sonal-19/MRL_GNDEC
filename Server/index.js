c2ccc871faec37fdcd0a76b836f20a4085fd512f
Sonal <sonalchy02@gmail.com>
2024-04-08 21:24:51 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://mr-lab.vercel.app",
+  origin: "https://mrl-gndec.vercel.app",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true, 
   optionSuccessStatus: 200,
